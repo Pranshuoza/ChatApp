4ff0039997522b79e43d479d06ae56f6aa885d69
Pranshu Oza <pranshuoza2004@gmail.com>
2025-02-10 11:26:21 +0530

    Final

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -6,7 +6,7 @@ config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API,
+  api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 });
 
