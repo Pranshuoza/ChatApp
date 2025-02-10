263a67d508dacfe0b2141e1b814a55eea206a832
Pranshu Oza <pranshuoza2004@gmail.com>
2025-02-10 12:32:40 +0530

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import path from "path";
 
@@ -16,23 +16,41 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages',messageRoute)
+app.get("/", (req, res) => {
+    res.send(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Hello</title>
+      </head>
+      <body>
+          <h1>Hello, welcome to my app!</h1>
+      </body>
+      </html>
+    `);
+  });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoute);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log("Server is running on port:" + PORT);
-    connectDB()
+  console.log("Server is running on port:" + PORT);
+  connectDB();
 });
