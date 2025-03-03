bdb4fd91442138ccbaecd1c0c5d087b6499a94de
Pranshu Oza <pranshuoza2004@gmail.com>
2025-03-03 20:18:18 +0530

    commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://chat-app-dun-three.vercel.app/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://chatapp-eh7e.onrender.com/api",
   withCredentials: true, 
   timeout: 60000, 
 });
